daababcb5bf4de4e546eee200467253e3d72afaa
hb <hubo@hbdeMac-mini.local>
2019-04-02 16:39:40 +0800

    proceeding class spectrum

--- spec/spec.py
+++ spec/spec.py
@@ -1,3 +1,8 @@
+import numpy as np
+import astropy.units as u
+
+__all__ = ['Spectrum']
+
 class Spectrum():
     '''
     Object for implement radio spectrum 
@@ -9,5 +14,11 @@ class Spectrum():
         
     '''
 
-    def __init__(self, freq, value)
+    def __init__(self, freq, value):
+        if not isinstance(freq, u.Quantity):
+            raise TypeError('freq must be an astropy.units.Quantity instance')
+        if not isinstance(value, u.Quantity):
+            raise TypeError('value must be an astropy.units.Quantity instance')
+        if freq.shape == value.shape:
+            raise ValueError('shape of freq and value are not identical {0} {1}'.format(freq.shape, value.shape))
 

de6857d194dd185e1828a939949387db07df66af
hb <hubo@hbdeMac-mini.local>
2019-04-03 16:57:21 +0800

    update spec.py

--- spec/spec.py
+++ spec/spec.py
@@ -14,11 +14,21 @@ class Spectrum():
         
     '''
 
-    def __init__(self, freq, value):
-        if not isinstance(freq, u.Quantity):
+    def __init__(self, x, y, **kwargs):
+        if not isinstance(x, u.Quantity):
             raise TypeError('freq must be an astropy.units.Quantity instance')
-        if not isinstance(value, u.Quantity):
-            raise TypeError('value must be an astropy.units.Quantity instance')
-        if freq.shape == value.shape:
+
+        if not isinstance(y, u.Quantity):
+            if not isinstance(y, np.ndarray):
+                raise TypeError('value must be an astropy.units.Quantity or numpy.ndarray instance')
+            else:
+                y = y * u.one
+
+        if x.shape != y.shape:
             raise ValueError('shape of freq and value are not identical {0} {1}'.format(freq.shape, value.shape))
+        
+        self.x = x 
+        self.y = y 
+        
+        if 'restfreq' in kwargs.keys():
 

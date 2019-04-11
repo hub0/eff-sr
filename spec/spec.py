73d470975fa14b86d96888a3cb2c65dc45d30f72
hb <hubo@hbdeMac-mini.local>
2019-04-11 17:23:13 +0800

    improving spec

--- spec/spec.py
+++ spec/spec.py
@@ -10,8 +10,12 @@ class Spectrum():
 
     Parameters
     ----------
-    x : '~astropy.Quantity' with frequency or unit
-    y : '~astropy.Quantity' with power or temperature unit
+    x : '~astropy.Quantity' with frequency or velocity unit
+    y : '~numpy.ndarray'
+    
+    kwargs
+    ------
+    restfreq : '~astropy.Quantity' with frequency unit
         
     '''
 
@@ -31,16 +35,29 @@ class Spectrum():
         self.x = x 
         self.y = y 
         
-        if x.units in [u.Hz, u.kHz, u.MHz, u.GHz]:
+        if x.unit in [u.Hz, u.kHz, u.MHz, u.GHz]:
             self.freq = x
             if 'restfreq' in kwargs.keys():
-                self.velo =  (1 - self.freq / kwargs['restfreq']) * const.c
-        elif x.units in [u.m/u.s, u.km/u.s]:
+                self.velo = (kwargs['restfreq'] - self.freq) / kwargs['restfreq'] * const.c
+        elif x.unit in [u.m/u.s, u.km/u.s]:
             self.velo = x
             if 'restfreq' in kwargs.keys():
-                self.freq = (1 - self.velo / const.c) * kwargs['restfreq']
+                self.freq = (const.c - self.velo) / const.c * kwargs['restfreq']
         else:
             raise ValueError('x must be in frequency or velocity unit')
+
+
+
+    def set_restfreq(self, restfreq):
+        '''
+        set rest frequency for self instance
+        '''
+        self.restfreq = restfreq
+        if 'freq' in self.__dict__:
+            self.velo = (1 - self.freq / restfreq) * const.c
+        if 'velo' in self.__dict__:
+            self.freq = (1 - self.velo / const.c) * restfreq
+
 
     def lsr_corr(self, vlsr):
         '''
@@ -56,12 +73,17 @@ class Spectrum():
         -------
         spect_shifted : an instance of Spectrum with shifted x axes
         '''
-        spec_shifted = self
-        spec_shifted.y = self.y
-        if 'freq' in self.__dict__:
-            spec_shifted.freq = self.freq * vlsr / const.c + self.freq
-        if 'velo' in self.__dict__:
-            spec_shifted.velo = self.velo - vlsr
+        if 'restfreq' not in self.__dict__:
+            raise ValueError('No rest frequency')
 
+        spec_shifted = self
+
+        spec_shifted.velo = self.velo - vlsr
+        spec_shifted.freq = (const.c - spec_shifted.velo) / const.c * self.freq
+
+        return spec_shifted
+
+
+    #def interpolate():
 
 

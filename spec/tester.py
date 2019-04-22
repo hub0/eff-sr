b31cbf9143d1fba147c02ba878f23cf613a24461
hub0 <hubonju@gmail.com>
2019-04-22 16:03:58 +0800

    improve spec

--- spec/tester.py
+++ spec/tester.py
@@ -3,9 +3,13 @@ import astropy.units as u
 from spec import *
 
 a = np.array([1,2,3,4,5])
-x = a * u.GHz
-y = a * 2
+b = a + np.array([ -0.1, 0.2, -0.1, -0.05, 0.2])
+x0 = a * u.GHz
+x1 = b * u.GHz
+y0 = a * 2
+y1 = np.ones(5)
 
-spec = Spectrum(x, y, restfreq=2*u.GHz)
+spec0 = Spectrum(x0, y0, restfreq=2*u.GHz)
+spec1 = Spectrum(x1, y1)
 
 
